6f1dc135e359fe959b9fab99f6c179ae9edc0246
lullaby6 <lucianobrumer5@gmail.com>
2025-02-18 12:19:43 -0300

    fixing public files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@
 
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from 'url';
 import express from "express";
 import cors from "cors";
 import ip from 'ip';
 import qrcode from 'qrcode-terminal'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const publicPath = path.join(__dirname, './public');
 
 import { port, storagePath } from "./config.js";
 
@@ -16,7 +22,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.use(express.static('./src/public'));
+app.use(express.static(publicPath));
 app.use('/storage', express.static(storagePath));
 
 if (!fs.existsSync(storagePath)) fs.mkdirSync(storagePath);
